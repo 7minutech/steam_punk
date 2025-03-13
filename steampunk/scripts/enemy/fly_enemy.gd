6d66b0cfe82b644fff5faaca2209dccd5ccbca79
7minutech <elijohnson1107@gmail.com>
2025-03-13 08:22:59 -0500

    feat: added test player and main scene and scripts

--- steampunk/scripts/enemy/fly_enemy.gd
+++ steampunk/scripts/enemy/fly_enemy.gd
@@ -3,29 +3,33 @@ extends Node2D
 var chasing_player = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$CharacterBody2D/AnimatedSprite2D.play("fly")
+	$AnimatedSprite2D.play("fly")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if chasing_player:
+		print("chasing player")
 		
-		
+	pass
+
+
+
+
+func flying_enemy():
 	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		chasing_player = true
-		return
+		print("chasing player now")
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		chasing_player = false
-		return
+		print("not chasing player now")
 	pass # Replace with function body.
-
-func flying_enemy():
-	pass
