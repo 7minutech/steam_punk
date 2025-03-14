c27fc7d40710518ebc5729d75782bfe7946076ec
7minutech <elijohnson1107@gmail.com>
2025-03-14 09:18:57 -0500

    feat: added testing for main

---
+++ steampunk/scripts/player/dummy_player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+const SPEED = 100
+const JUMP_VELOCITY = -400.0
+func _ready() -> void:
+	var direction = Vector2.LEFT
+	velocity = direction * SPEED
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	
+func player():
+	pass
