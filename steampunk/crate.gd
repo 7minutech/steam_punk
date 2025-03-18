eb1903a33f24081cac719f0f5b82d4ebf9dec30b
mateipalcau <mpalcau@stu.jsu.edu>
2025-03-17 21:12:35 -0500

    I created a crate that can take damage, and after it reaches a certain level of damage, it dissapears and instead of it a weapon appears. To help me test this feature, I created a temporary player, that shoots bullets, and a temporary main scene. The crate dissapears after the player shoots 5 times in the crate, and instead of the crate, a weapon appears

--- steampunk/crate.gd
+++ steampunk/crate.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
-
-var health: int = 50  
+var health: int = 50 
+var heart_scene = preload("res://temporary_weapon.tscn") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +11,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
+
 func take_damage(amount: int):
 	health -= amount
 	if health <= 0:
-		die()
+		remove()
 		
-func die():
+func remove():
+	if heart_scene:
+		var coin = heart_scene.instantiate()  
+		coin.position = global_position 
+		get_parent().add_child(coin)  
 	queue_free() 
