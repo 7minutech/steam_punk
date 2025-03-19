a02af8de3274df1b575bf76bd22df68854488ab4
7minutech <elijohnson1107@gmail.com>
2025-03-19 08:40:09 -0500

    feat: added new scenes and assests for bullet and gun

---
+++ steampunk/scripts/player/bullet.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var speed = 1500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	top_level
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#fire bullet straight in the direction the gun is pointed
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+	pass
+
+func _physics_process(delta: float) -> void:
+	#flash 
+	await get_tree().create_timer(0.01).timeout
+	#actual bullet
+	$Sprite2D.frame = 1
+	set_physics_process(false)
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.has_method("player"):
+		queue_free()
+	pass # Replace with function body.
---
+++ steampunk/scripts/player/gun.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+var can_fire: bool = true
+var bullet = preload("res://scenes/player/Bullet.tscn")
+@export var bullet_cd: float = 0.2
+@onready var screen_shaker = $Camera2D/ScreenShaker
+
+func _ready() -> void:
+	top_level
+
+func _physics_process(delta: float) -> void:
+	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
+	
+	if Input.is_action_pressed("fire") and can_fire:
+		var bullet_instace = bullet.instantiate()
+		bullet_instace.rotation = rotation + randf_range(-0.1,0.1)
+		bullet_instace.position = $Muzzle.position
+		get_parent().get_node("Camera2D/ScreenShaker").shake(0.2,2)
+		get_parent().add_child(bullet_instace)
+		can_fire = false
+		await get_tree().create_timer(bullet_cd).timeout
+		can_fire = true
+		pass
