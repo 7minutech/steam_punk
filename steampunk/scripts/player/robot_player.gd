dc8060edefe6f743997c7488a3208c196cce3f15
7minutech <elijohnson1107@gmail.com>
2025-03-27 16:36:15 -0500

    feat: improved character sprite logic

--- steampunk/scripts/player/robot_player.gd
+++ steampunk/scripts/player/robot_player.gd
@@ -4,9 +4,10 @@ extends CharacterBody2D
 @export var speed: float = 200.0
 const JUMP_VELOCITY: float = -400.0 
 @export var gravity: bool = true
+var running: bool = false
+var idling: bool = false
 func _ready() 	-> void:
 	Global.player = self
-	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -25,15 +26,35 @@ func _physics_process(delta: float) -> void:
 	if direction < 0:
 		$AnimatedSprite2D.flip_h = true
 		$Gun.flip_h = true
-	else:
+	if direction > 0:
 		$AnimatedSprite2D.flip_h = false
 		$Gun.flip_h = false
 	if direction:
+		running = true
+		run()
+		idling = false
 		velocity.x = direction * speed
 	else:
+		idling = true
+		idle()
+		running = false
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 	
 func player() -> void:
 	return
+
+func run() -> void:
+	if running:
+		if idling:
+			$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("run")
+
+func idle() -> void:
+	if idling:
+		if running:
+			$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idle")
+	
+		
