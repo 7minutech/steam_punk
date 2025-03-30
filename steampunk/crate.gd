e40b80887834856937f7bb17d84f653b5c767e7b
mateipalcau <mpalcau@stu.jsu.edu>
2025-03-30 15:22:32 -0500

    crate feature is done

---
+++ steampunk/crate.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var health: int = 50
+var gun_scene = preload("res://player_gun.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		remove()
+		
+func remove():
+	if gun_scene:
+		var gun = gun_scene.instantiate()
+		gun.position = global_position
+		get_parent().add_child(gun)
+	queue_free()
