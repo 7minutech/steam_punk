a7768c4bcf628d49c951e4adcf473ac1149c5f2a
7minutech <elijohnson1107@gmail.com>
2025-04-03 16:39:19 -0500

    feat: platforms dissappear and reappear

--- steampunk/scripts/object/dissapearing_platforms.gd
+++ steampunk/scripts/object/dissapearing_platforms.gd
@@ -1,6 +1,7 @@
 extends AnimatableBody2D
 
-
+@export var time_to_disappear: float = 1.0
+@export var time_to_appear: float = 1.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,11 +12,23 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_timer_timeout() -> void:
-	queue_free()
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("hero"):
 		print("player entered area")
-		$Timer.start()
+		$DisapperTimer.start()
+
+
+func _on_disapper_timer_timeout() -> void:
+	$sprite2d.hide()
+	$CollisionShape2D.disabled = true
+	$AppearTimer.start()
+	pass # Replace with function body.
+
+
+func _on_appear_timer_timeout() -> void:
+	$sprite2d.show()
+	$CollisionShape2D.disabled = false
+	pass # Replace with function body.
